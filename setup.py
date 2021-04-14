56aed9b98eeab6d5bea4b347d6ce9fc26ba6a16e
Youhee <youhee.kil@gmail.com>
2021-04-14 22:32:49 +0200

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a final project for MDA course in KU LEUVEN (2021)',
+    author='MDA_philippines',
+    license='MIT',
+)
